b954f48e9b6820fffa95f184a92f182b4524b486
TheeChody <chodywhooch@live.ca>
2025-11-20 23:47:51 -0700

    Initial from pycharm

--- chodeling.py
+++ chodeling.py
@@ -21,6 +21,7 @@ from mongoengine import connect, disconnect_all, DEFAULT_CONNECTION_NAME, Docume
 
 # ToDo; Add 'upsidedown text to everything' easter egg
 # ToDo; Add stats for time added (also rework chodebot & user documents to keep track & write script to scrape past logs to build data)
+#
 
 if getattr(sys, 'frozen', False):
     folder_name = "Thee ChodelingApp"
